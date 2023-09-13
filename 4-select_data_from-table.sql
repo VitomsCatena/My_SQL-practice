95d4c7d2d05ad7a31e331f5614b05ee52272742f
VitomsCatena <105198927+VitomsCatena@users.noreply.github.com>
2023-09-13 14:53:22 +0300

    Update 4-select_data_from-table.sql

--- 4-select_data_from-table.sql
+++ 4-select_data_from-table.sql
@@ -5,3 +5,7 @@ CREATE TABLE friends(
   money_daily DECIMAL (6,2),
   pay_day DATE
 );
+--To select all from table--
+SELECT *FROM friends;
+
+/*this will select all the content in my friends table*/
