bfcccb5720a6553801268144dc8f03091639c9f9
VitomsCatena <105198927+VitomsCatena@users.noreply.github.com>
2023-09-13 14:50:27 +0300

    inserting-rows_in_table

--- 3-inserting-rows_in_table.sql
+++ 3-inserting-rows_in_table.sql
@@ -9,11 +9,18 @@ CREATE TABLE friends(
 );
 
 --to insert a row in table above--
+
 INSERT INTO friends
 VALUES (1, "Samwel" , "Mwas" , 250.65 , "2021-12-02");
 
 --to insert multiple rows at once--
+
 INSERT INTO friends
 VALUES (1, "Victor" , "Onyango" , 367.9 , "2004-12-1"),
-      (1, "Ivy" , "Njeri" , 267.9 , "2009-12-1"),
-      (1, "Victor" , "Onyango" , 367.9 , "2004-12-1"),(1, "Victor" , "Onyango" , 367.9 , "2004-12-1");
+       (1, "Ivy" , "Njeri" , 267.9 , "2009-12-1"),
+       (1, "Victor" , "Onyango" , 367.9 , "2004-12-1"),
+       (1, "Victor" , "Onyango" , 367.9 , "2004-12-1");
+
+--to insert on a specific row--
+
+INSERT INTO friends (friends_id , first_name , last_name) /* you specify the row*/
