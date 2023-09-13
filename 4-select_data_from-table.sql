78db8abe9dadf4bc828382adee55c145747f0c3b
VitomsCatena <105198927+VitomsCatena@users.noreply.github.com>
2023-09-13 15:00:51 +0300

    Update 4-select_data_from-table.sql

--- 4-select_data_from-table.sql
+++ 4-select_data_from-table.sql
@@ -10,8 +10,8 @@ CREATE TABLE friends(
 INSERT INTO friends
 VALUES (1, "Victor" , "Onyango" , 367.9 , "2004-12-1"),
        (1, "Ivy" , "Njeri" , 267.9 , "2009-12-1"),
-       (1, "Victor" , "Onyango" , 367.9 , "2004-12-1"),
-       (1, "Victor" , "Onyango" , 367.9 , "2004-12-1");
+       (1, "Victor" , "Omondi" , 300.9 , "2004-12-1"),
+       (1, "Victoria" , "Onyango" , 267.9 , "2004-12-1");
 /*TABLE END*/
 
 --To select all from table--
@@ -19,5 +19,6 @@ SELECT *FROM friends;
 /*this will select all the content in my friends table*/
 
 --to select with criteria--
+--this will select all the data with the name Njeri--
 SELECT * FROM friends
 WHERE last_name = "Njeri"
