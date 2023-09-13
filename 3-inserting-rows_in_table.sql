4cfd5551469d91a4357194c54cf142a3f8bb944e
VitomsCatena <105198927+VitomsCatena@users.noreply.github.com>
2023-09-13 14:43:34 +0300

    Update 3-inserting-rows_in_table.sql

--- 3-inserting-rows_in_table.sql
+++ 3-inserting-rows_in_table.sql
@@ -11,3 +11,7 @@ CREATE TABLE friends(
 --to insert a row in table above--
 INSERT INTO friends
 VALUES (1, "Samwel" , "Mwas" , 250.65 , "2021-12-02");
+
+--to insert multiple rows at once--
+INSERT INTO friends
+VALUES (),(),(),(),()
