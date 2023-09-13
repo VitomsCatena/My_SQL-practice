053776368e05510a459412d4388ad7dbab70cfd2
VitomsCatena <105198927+VitomsCatena@users.noreply.github.com>
2023-09-13 14:58:06 +0300

    Update 4-select_data_from-table.sql

--- 4-select_data_from-table.sql
+++ 4-select_data_from-table.sql
@@ -1,3 +1,4 @@
+/*TABLE BEGINS*/
 CREATE TABLE friends(
   friend_id  INT,
   first_name  VARCHAR(60),
@@ -5,6 +6,14 @@ CREATE TABLE friends(
   money_daily DECIMAL (6,2),
   pay_day DATE
 );
+
+INSERT INTO friends
+VALUES (1, "Victor" , "Onyango" , 367.9 , "2004-12-1"),
+       (1, "Ivy" , "Njeri" , 267.9 , "2009-12-1"),
+       (1, "Victor" , "Onyango" , 367.9 , "2004-12-1"),
+       (1, "Victor" , "Onyango" , 367.9 , "2004-12-1");
+/*TABLE END*/
+
 --To select all from table--
 SELECT *FROM friends;
 /*this will select all the content in my friends table*/
