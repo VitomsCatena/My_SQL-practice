ad4df3862e121e5d850b981ec6df65f1c1f8e623
VitomsCatena <105198927+VitomsCatena@users.noreply.github.com>
2023-09-13 14:41:56 +0300

    Update 3-inserting-rows_in_table.sql

--- 3-inserting-rows_in_table.sql
+++ 3-inserting-rows_in_table.sql
@@ -1,3 +1,13 @@
 --Here is my table--
 
+CREATE TABLE friends(
+  friend_id  INT,
+  first_name  VARCHAR(60),
+  last_name   VARCHAR(60),
+  money_daily DECIMAL (6,2),
+  pay_day DATE
+);
 
+--to insert a row in table above--
+INSERT INTO friends
+VALUES (1, "Samwel" , "Mwas" , 250.65 , "2021-12-02");
